5bf2b8b0f5bc8ada95418669200b78bc9d761f77
Renju Jose <renjosevadakael@gmail.com>
2025-08-31 17:40:02 +0530

    Add Assignment1 and Assignment2

---
+++ Assignment2/Condition.py
@@ -0,0 +1,6 @@
+age = int(input("Enter your age: "))
+yearsOfITExperience = int(input("Enter your years of IT experience: ")) 
+if(age >=22 and yearsOfITExperience>=2):
+    print("Access Granted")
+else:
+    print("Access Denied")
